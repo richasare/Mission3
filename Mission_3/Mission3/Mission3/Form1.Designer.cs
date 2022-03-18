e228dcbed0584b2fe9be3b306fb458fb2730a377
richasare <asarerichemond@gmail.com>
2022-03-18 09:47:05 +0100

    phase 1

---
+++ Mission_3/Mission3/Mission3/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Mission3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.insertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tbId = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.insertToolStripMenuItem,
+            this.deleteToolStripMenuItem,
+            this.updateToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // insertToolStripMenuItem
+            // 
+            this.insertToolStripMenuItem.Name = "insertToolStripMenuItem";
+            this.insertToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.insertToolStripMenuItem.Text = "Insertion";
+            this.insertToolStripMenuItem.Click += new System.EventHandler(this.insertToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.deleteToolStripMenuItem.Text = "Deletion";
+            // 
+            // updateToolStripMenuItem
+            // 
+            this.updateToolStripMenuItem.Name = "updateToolStripMenuItem";
+            this.updateToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.updateToolStripMenuItem.Text = "Update";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(521, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(521, 360);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 36);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(521, 402);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(165, 36);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(586, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(586, 200);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // tbId
+            // 
+            this.tbId.Location = new System.Drawing.Point(586, 135);
+            this.tbId.Name = "tbId";
+            this.tbId.Size = new System.Drawing.Size(100, 20);
+            this.tbId.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(586, 254);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // dgv1
+            // 
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Location = new System.Drawing.Point(53, 72);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.Size = new System.Drawing.Size(317, 282);
+            this.dgv1.TabIndex = 9;
+            this.dgv1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(533, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgv1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.tbId);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem insertToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox tbId;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Mission_3/Mission3/Mission3/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Connecté;
+using MySql.Data.MySqlClient;
+
+namespace Mission3
+{
+    public partial class Form1 : Form
+    {
+        private string provider = "localhost";
+        private string nomBdd = "SicilyLines";
+        private string uid = "root";
+        private string mdp = "";
+
+        private ConnexionSql maConnexionSql;
+        private MySqlCommand oCom, com2;
+        private DataTable dt;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void insertToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            maConnexionSql = ConnexionSql.getInstance(provider, nomBdd, uid, mdp);
+            maConnexionSql.OpenConnection();
+            affiche();
+        }
+
+        public void affiche()
+
+        {
+            try
+            {
+
+                dt = new DataTable();
+
+                oCom = maConnexionSql.reqExec("Select nom, libelle from liaison natural join port natural join secteur");
+
+                //      MySqlDataReader reader = oCom.ExecuteReader();
+
+                MySqlDataAdapter myDataAdapter = new MySqlDataAdapter(oCom);
+
+                myDataAdapter.Fill(dt);
+
+                dgv1.DataSource = dt;
+
+                /*
+
+                                for (int i = 0; i <= reader.FieldCount - 1; i++)
+                                {
+
+                                    dt.Columns.Add(reader.GetName(i));
+                                }
+
+
+                                while (reader.Read())
+                                {
+
+
+
+
+                                    DataRow dr = dt.NewRow();
+
+
+                                    for (int i = 0; i <= reader.FieldCount - 1; i++)
+                                    {
+
+                                        dr[i] = reader.GetValue(i);
+                                    }
+
+                                    dt.Rows.Add(dr);
+
+                                }
+
+                    */
+
+
+                // DATAGRIDVIEW
+                //dgv1.DataSource = dt;
+
+                //     dgv1.Columns[0].ReadOnly = true;
+
+                //    reader.Close();
+
+
+            }
+
+
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+
+
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                string req = "update liaison set duree = '" + textBox1.Text + "' where id = " + tbId.Text;
+
+                com2 = maConnexionSql.reqExec(req);
+
+                int affectedrows = com2.ExecuteNonQuery();
+
+                affiche();
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            string req = "delete from liaison where id = " + tbId.Text;
+
+            com2 = maConnexionSql.reqExec(req);
+
+            int affectedrows = com2.ExecuteNonQuery();
+
+            affiche();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // string req = "insert into employe1 values (" + tId.Text + ",'" + tLogin.Text + "')";
+
+
+            // requête paramétrée
+            string req = "insert into liaison(id,duree,port_depart_id, port_arrivee_id, le_secteur_id) values ("+tbId.Text+","+textBox1.Text+","+textBox2.Text+","+textBox4.Text+")";
+
+
+
+
+
+            com2 = maConnexionSql.reqExecParametree(req);
+
+
+            com2.Parameters.Add("@id", MySqlDbType.Int32, 3);
+
+            com2.Parameters["@id"].Value = Convert.ToInt32(tbId.Text);
+
+
+            com2.Parameters.Add("@login", MySqlDbType.VarChar, 30);
+
+            com2.Parameters["@login"].Value = textBox1.Text;
+
+
+
+            int affectedrows = com2.ExecuteNonQuery();
+
+            affiche();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            maConnexionSql.CloseConnection();
+        }
+
+    }
+}
